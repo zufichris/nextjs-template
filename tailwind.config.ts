7ea3666c9581d7bea5c397626a05ca6e1d46a473
{zufichris} <{zufichris@gmail.com}>
2024-12-21 06:00:17 +0400

    TailwindCss configuraton

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,11 @@
-import type { Config } from "tailwindcss";
-
-export default {
+import {Config} from "tailwindcss"
+import theme from "./theme"
+export  default {
   content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
+   ],
+  theme,
   plugins: [],
-} satisfies Config;
+} as Config
